144be8dfea46314a4da174d89667c10065880c78
Ajay Singh <ajay87891@gmail.com>
2023-07-16 17:43:42 +0530

    2nd

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head"
+import Navbar from "../components/navbar"
+
+export default function Home() {
+
+
+  return (
+    <>
+    <div>
+    <Head>
+      <title>RK Sweets</title>
+    </Head>
+
+    </div>
+    <div className="bg-slate-200">
+        <h1 className="text-slate-950">hello</h1>
+        <Navbar>
+
+        </Navbar>
+      
+    </div>
+    </>
+  )
+}
